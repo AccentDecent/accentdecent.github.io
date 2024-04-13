caf871a035dac289fffcc3de7812a5384ee73f0e
CorruptionHades <corruptionhades@gmail.com>
2024-04-13 18:49:55 +0200

    [+] Continued on latin + styling

--- latin_translator/script.js
+++ latin_translator/script.js
@@ -1,3 +1,6 @@
+const infoText = document.getElementById("info_text");
+const displayBox = document.getElementById("displayBox");
+
 document.getElementById("test").addEventListener("click", async function() {
     console.log("now pinging...");
    await sendRequest(document.getElementById("linkInput").value);
@@ -20,7 +23,65 @@ async function sendRequest(info) {
 
     console.log(text);
 
-    document.getElementById("displayBox").innerHTML = text;
+    displayBox.innerHTML = "";
+
+    if(contains(text, "Kurzübersicht")) {
+        infoText.innerText = "Richtige saite";
+
+        const table = findTable(text);
+
+        if(table) {
+            for (let i = 2; i < table.rows.length; i++) {
+                const row = table.rows[i];
+
+                const lines = row.textContent.split("\n");
+
+                for (let j = 1; j < lines.length; j++) {
+                    const line = lines[j];
+
+                    if(line.includes(info)) {
+                        continue;
+                    }
+
+                    displayBox.innerText += line + "\n";
+                }
+
+                console.log(row);
+            }
+        }
+    }
+    else {
+        infoText.innerText = "mehreree möglichkweiten";
+        displayBox.innerHTML = text;
+    }
+
+
+}
+
+function contains(html, search) {
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(html, 'text/html');
+
+    let allElements = doc.body.getElementsByTagName('*');
+    for (let i = allElements.length - 1; i >= 0; i--) {
+
+        const title = allElements[i].title;
+
+        if (title !== undefined && allElements[i].innerText.includes(search)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function findTable(html) {
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(html, 'text/html');
+
+    let table = doc.querySelector('.table-responsive table');
+
+    return table;
 }
 
 function removeUseless(html) {
@@ -39,7 +100,10 @@ function removeUseless(html) {
     // Remove any tags that contain the string "im Lateinwörterbuch"
     let allElements = doc.body.getElementsByTagName('*');
     for (let i = allElements.length - 1; i >= 0; i--) {
-        if (allElements[i].title.includes("im Lateinwörterbuch")) {
+
+        const title = allElements[i].title;
+
+        if (title !== undefined && title.includes("im Lateinwörterbuch")) {
             allElements[i].parentNode.removeChild(allElements[i]);
         }
     }
